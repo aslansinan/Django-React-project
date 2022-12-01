af14c29517665376dc569ada57a639f96a8cb1b4
sinan <aslansinan586@gmail.com>
2022-12-01 22:08:45 +0300

    add-entity-framework

--- music_app/api/urls.py
+++ music_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import main
+from .views import RoomView
 
 urlpatterns = [
-    path('home',main)
+    path('room',RoomView.as_view())
 ]
+ 
